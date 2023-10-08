10df7c3136ebfbe8085a14089a8551e2872ccf57
Joshua Owusu <joshbillion1@gmail.com>
2023-10-08 20:55:09 +0000

    removed gsap color change

--- script.js
+++ script.js
@@ -20,12 +20,3 @@ var typed = new Typed(".auto-type", {
   backSpeed: 150,
   loop: true,
 });
-
-function changeBackground() {
-  if (window.scrollY > window.innerHeight / 2) {
-    document.body.classList.add("bg-color");
-  } else {
-    document.body.classList.remove("bg-color");
-  }
-}
-window.addEventListener("scroll", changeBackground);
